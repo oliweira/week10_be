3f0761719f850172b05363c59e18910757d456f3
Oliweira <oliweira@gmail.com>
2020-02-25 17:54:21 -0300

    adicionando a conexão do banco no mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 //Gerencias as rotas
 const express = require("express");
+//Gerencia a conexão da aplicação com o mongod
 const mongoose = require("mongoose");
 const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/week10', {
+//mongoose.connect('mongodb://localhost:27017/week10', {
+//mongoose.connect('mongodb+srv:admin:admin@cluster0-qrlhh.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://admin:admin@cluster0-shard-00-00-qrlhh.mongodb.net:27017,cluster0-shard-00-01-qrlhh.mongodb.net:27017,cluster0-shard-00-02-qrlhh.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
